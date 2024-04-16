a5a6adcdc5ea81be16fb7ab22901a17ce79e32ed
ittamalla2604 <ittamalla2604@gmail.com>
2024-04-16 22:29:44 +0530

    some-message

---
+++ ifconditionpass.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+######
+
+######
+
+echo "Enter the user name"
+
+read username
+
+echo "Enter the password"
+
+read -s password
+
+if [ $password == 'password']
+
+then 
+
+echo "password matched and you are good to go"
+
+else 
+
+echo "please type the correct password"
+
+fi
